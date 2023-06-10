90a9d28a95ec00a10e5009326a03567d2c239dbf
sitonaka <csitonaka@gmail.com>
2023-06-11 07:10:11 +0900

    Initial Commit

---
+++ MyApplication2/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  MyApplication2
+//
+//  Created by no name on 2023/06/11.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyApplication2/MyApplication2App.swift
@@ -0,0 +1,18 @@
+//
+//  MyApplication2App.swift
+//  MyApplication2
+//
+//  Created by no name on 2023/06/11.
+//  
+//
+
+import SwiftUI
+
+@main
+struct MyApplication2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
