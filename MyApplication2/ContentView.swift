b587562d3c61e16e0221659489cca0ce043bdd76
sitonaka <csitonaka@gmail.com>
2023-07-08 19:59:22 +0900

    step 4.

--- MyApplication2/ContentView.swift
+++ MyApplication2/ContentView.swift
@@ -15,6 +15,9 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, iPhone!")
+            Button("Push me !") {
+                
+            }
         }
         .padding()
     }
